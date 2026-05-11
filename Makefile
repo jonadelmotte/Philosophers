8753a560389665b33c81519cbc4806feca2b78c7
jdelmott <jdelmott@f0r8s20.paris.42.school>
2026-05-11 18:11:29 +0200

    lol

--- Makefile
+++ Makefile
@@ -15,25 +15,34 @@ OBJ = $(addprefix $(OBJ_DIR), $(addsuffix .o, $(FILES)))
 
 OBJF = .cache_exits
 
+GREEN = \033[0;92m
+YELLOW = \033[33m
+BLUE = \033[34m
+RESET = \033[0;39m
+
 $(OBJF):
-	mkdir -p $(OBJ_DIR)
+	@mkdir -p $(OBJ_DIR)
+	@echo -e "$(BLUE)Obj directory created$(RESET)"
 
 vpath %.c $(SRC_DIR)
 
 all: $(NAME)
 
 $(NAME): $(OBJ)
-	$(CC) $(FLAGS) $(OBJ) -g3 -o $(NAME)
+	@$(CC) $(FLAGS) $(OBJ) -g3 -o $(NAME)
+	@echo -e "$(GREEN)Philosophers compiled !!$(RESET)"
 
 $(OBJ_DIR)%.o: %.c $(INCLUDE) Makefile | $(OBJF)
-	$(CC) $(FLAGS) -c -g3 $< -o $@
+	@$(CC) $(FLAGS) -c -g3 $< -o $@
 
 clean:
-	rm -rf $(OBJ)
-	rm -rf $(OBJ_DIR)
+	@rm -rf $(OBJ)
+	@rm -rf $(OBJ_DIR)
+	@echo -e "$(YELLOW)Obj destroyed !!$(RESET)"
 
 fclean: clean
-	rm -f $(NAME)
+	@rm -f $(NAME)
+	@echo -e "$(YELLOW)Executable destroyed !!$(RESET)"
 
 re: fclean all
 
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jdelmott <jdelmott@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 17:10:17 by jdelmott          #+#    #+#             */
-/*   Updated: 2026/05/07 17:10:35 by jdelmott         ###   ########.fr       */
+/*   Updated: 2026/05/11 17:43:48 by jdelmott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	main(int argc, char *argv[])
 	t_pars	pars;
 
 	if (argc > 6 || argc < 5)
-		return (printf("Not the right amount of args\n"), 1);
+		return (printf("%sNot the right amount of args\n%s", RED, RESET), 1);
 	if (parser(argv, argc, &pars))
-		return (printf("Invalide args\n"), 1);
-	printf("\e[1;34mnb_philo = %i, nb_fork = %i, t_die = %i, t_eat = %i, t_sleep = %i, num_eat = %i\n$", pars.nb_philo, pars.nb_fork, pars.t_die, pars.t_eat, pars.t_sleep, pars.num_eat);
+		return (printf("%sInvalide args\n%s", RED, RESET), 1);
+	printf("%snb_philo = %i, nb_fork = %i, t_die = %i, t_eat = %i, t_sleep = %i, num_eat = %i\n%s", GREEN, pars.nb_philo, pars.nb_fork, pars.t_die, pars.t_eat, pars.t_sleep, pars.num_eat, RESET);
+	printf("yolo\n");
 }
